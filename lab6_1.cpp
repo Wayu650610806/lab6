22917199dc119ecaa55966193b6dd903e13a7c6e
= <=>
2022-12-09 15:26:48 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num=1;
+    int odd=0;
+    int even=0;
+    while(num!=0){
+        cout << "Enter an integer: ";
+        cin >> num;
+        if (num%2 == 0&&num!=00){
+            even++;
+        }
+        if(num%2 !=0){
+            odd++;
+        }
+
+    
+    
+    }
+    
+    
+    cout << "#Even numbers = "<< even <<"\n";
+    cout << "#Odd numbers = "<<odd <<"\n";
     return 0;
 }
